cdf17a7fa7751f8a8405fac86bb0bf7963b71830
swathigakathirvelProject02PMJuly31 <swathigaeee@gmail.com>
2021-08-03 11:22:05 +0530

    Project

---
+++ src/org/cts/test/login/CourseDetails.java
@@ -0,0 +1,21 @@
+package org.cts.test.login;
+
+public class CourseDetails
+{
+private void javacourse() 
+{
+	System.out.println("java course starts today");
+}
+private void seleniumcourse() {
+	System.out.println("selenium course starts tomorrow");
+}
+public static void main(String[] args) 
+{
+CourseDetails c=new CourseDetails();
+c.javacourse();
+c.seleniumcourse();
+}
+}
+
+
+
